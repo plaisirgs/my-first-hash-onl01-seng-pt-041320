81e02b68b1270fdbc88c05011cabc16fb3c4ac92
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-06-18 09:26:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,14 +16,3 @@ def adding
   the_manifest_two = {"muskets" => 2, "gun powder" => 4}
   the_manifest.merge!(the_manifest_two)
 end
-# h1 = { "a" => 100, "b" => 200 }
-# h2 = { "b" => 254, "c" => 300 }
-# h1.merge!(h2)   #=> {"a"=>100, "b"=>254, "c"=>300}
-
-# h1 = { "a" => 100, "b" => 200 }
-# h2 = { "b" => 254, "c" => 300 }
-# h1.merge!(h2) { |key, v1, v2| v1 }
-#                 #=> {"a"=>100, "b"=>200, "c"=>300}
-
-# the_manifest_two = {"maskets" => 2, "gun powder" => 4}
-  # the_manifest.merge!(the_manifest_two)
